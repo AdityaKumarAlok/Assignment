95cce212ddb2cc35545d8c795851fb1441e29737
Aditya <officialadityakumaralok@gmail.com>
2023-07-15 00:57:23 +0530

    Some Bootstrap And Css Done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const Card = ({ name, id,avatar,last_name }) => {
+const Card = ({ name, id, avatar, last_name }) => {
   return (
-    
-  <div class="card" >
-  <img src={avatar} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">{name} {last_name}</h5>
-  </div>
-</div>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <div class="card" >
+            <img src={avatar} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">{name} {last_name}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
   );
 }
